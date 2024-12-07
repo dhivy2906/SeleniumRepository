cc638d16bcbec1d67b0518f841d19e4e31e17810
dhivy2906 <118151232+dhivy2906@users.noreply.github.com>
2024-12-07 23:31:55 +0530

    Add files via upload
    Week1Day1

---
+++ week1/day1/WaterBottle.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class WaterBottle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int capacity = 130;
+		System.out.println("The waterbottle Capacity1 is " +capacity+"ml");
+		short capacity2 = 2000;
+		System.out.println("The waterbottle capacity2 is " +capacity2);
+		long phonenumber = 9234234234l;
+		System.out.println("The phonenumber is "+phonenumber);
+		float radius = 1.5f;
+		System.out.println("Tje radius value is " +radius);
+		double pieValue = 3.14444;
+		System.out.println("The pie value is "+pieValue);
+		String waterbottleColor = "pink";
+		System.out.println("The waterbottle colour is "+waterbottleColor);
+		char logo = 't';
+		System.out.println("The logo type is "+logo);
+		boolean isEmpty = false;
+		System.out.println("The boolean value is "+isEmpty);
+	}
+
+}
