2ba08e30ed4a00f25951fd0e0609ff3db23668f2
dhivy2906 <118151232+dhivy2906@users.noreply.github.com>
2024-12-07 23:48:00 +0530

    Add files via upload
    added week1day1 assignment

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version = 91.0f;
+		System.out.println("The Chrome version is "+version);
+		String developer = "Google";
+		System.out.println("The developer is "+developer);
+		boolean isBeta = false;
+		System.out.println("Chrome supports beta is "+isBeta);
+		int releaseYear = 2008;
+		System.out.println("The chrome release version is "+releaseYear);
+		char shortcutKey = 'C';
+		System.out.println("The short cut key is "+shortcutKey);
+	}
+
+}
